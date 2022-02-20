63d6974eeb43ac078ada6fffd21a28823498b90e
%Sunrise Server% <alzex3@gmail.com>
2022-02-20 13:45:38 +0300

    New

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -1,3 +1,5 @@
+import django_heroku
+
 """
 Django settings for stocks_products project.
 
@@ -137,3 +139,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+django_heroku.settings(locals())
